d61265b308ca2ebfdb9149bced0a4a1618befefc
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2020-04-22 12:33:28 +0200

    Ref more info

--- src/main/java/io/github/oliviercailloux/sample_flows/ZipFileCreator.java
+++ src/main/java/io/github/oliviercailloux/sample_flows/ZipFileCreator.java
@@ -13,6 +13,11 @@ public class ZipFileCreator {
 		URI zipFileUri = zipFilePath.toUri();
 		URI jarZipFileUri = URI.create("jar:" + zipFileUri.toString());
 
+		/**
+		 * See
+		 * https://docs.oracle.com/javase/7/docs/technotes/guides/io/fsp/zipfilesystemprovider.html
+		 * for reference information.
+		 */
 		ImmutableMap<String, String> env = ImmutableMap.of("create", "true");
 		try (FileSystem fs = FileSystems.newFileSystem(jarZipFileUri, env)) {
 			Path rootInsideZip = fs.getPath("");
