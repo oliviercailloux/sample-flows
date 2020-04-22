2373cd1eaf0712f8016f5deae07f05acd9aed6c3
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2020-04-22 12:06:04 +0200

    Sample create zip file

---
+++ src/main/java/io/github/oliviercailloux/sample_flows/ZipFileCreator.java
@@ -0,0 +1,22 @@
+package io.github.oliviercailloux.sample_flows;
+
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ZipFileCreator {
+	public static void main(String[] args) throws Exception {
+		Path zipFilePath = Path.of("anewzipfile.zip");
+		URI zipFileUri = zipFilePath.toUri();
+		URI jarZipFileUri = URI.create("jar:" + zipFileUri.toString());
+
+		Map<String, String> env = new HashMap<>();
+		env.put("create", "true");
+		try (FileSystem fs = FileSystems.newFileSystem(jarZipFileUri, env)) {
+			Path rootInsideZip = fs.getPath("");
+		}
+	}
+}
